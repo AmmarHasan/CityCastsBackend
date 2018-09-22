e5374ccdcd810a366a2dda166bdbefd1e8b244f0
Ammar Hasan <hasanammar94@gmail.com>
2018-09-23 00:09:02 +0200

    Remove AWS keys and change bucket name

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,8 +4,6 @@ const AWS = require('aws-sdk');
 const config = require('./../config');
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: config.REGION,
 });
 
